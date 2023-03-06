89478406197aceed7b1882296f49d04de44b3948
lRozx <1010137068@u.icesi.edu.co>
2023-03-05 21:16:38 -0500

    Added analytics to vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
